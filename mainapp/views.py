0a589033777a97cebce4b5b7d99a14a4e7111eb0
Nikita Cherenkov <17rolex@gmail.com>
2021-06-24 15:08:41 +0300

    lesson2

--- mainapp/views.py
+++ mainapp/views.py
@@ -2,12 +2,29 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'mainapp/index.html')
+    context = {
+        'title': 'Магазин'
+    }
+    return render(request, 'mainapp/index.html', context=context)
 
 
 def contact(request):
-    return render(request, 'mainapp/contact.html')
+    context = {
+        'title': 'Контакты'
+    }
+    return render(request, 'mainapp/contact.html', context=context)
 
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    links_menu = [
+        {'href': '#', 'name': 'все'},
+        {'href': '#', 'name': 'дом'},
+        {'href': '#', 'name': 'офис'},
+        {'href': '#', 'name': 'модерн'},
+        {'href': '#', 'name': 'классика'}
+    ]
+    context = {
+        'title': 'Продукты',
+        'links_menu': links_menu
+    }
+    return render(request, 'mainapp/products.html', context=context)
