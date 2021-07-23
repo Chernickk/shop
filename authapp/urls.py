a3ba08f369c6d7478aad7175543f2aa15effa2f7
Nik <17rolex@gmail.com>
2021-07-23 15:15:34 +0300

    les2.1

--- authapp/urls.py
+++ authapp/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('logout/', views.logout, name='logout'),
     path('register/', views.register, name='register'),
     path('edit/', views.edit, name='edit_profile'),
+    path('verify/<email>/<activation_key>/', views.verify, name='verify')
 ]
