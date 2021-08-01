c090c59d405b4fb0aa44da689318657150c7fe72
Nik <17rolex@gmail.com>
2021-08-01 20:04:45 +0300

    lesson2.3

---
+++ ordersapp/forms.py
@@ -0,0 +1,35 @@
+from django import forms
+from ordersapp.models import Order, OrderStep, OrderItem
+
+
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        exclude = ('user',)
+
+    def __init__(self, *args, **kwargs):
+        super(OrderForm, self).__init__(*args, **kwargs)
+        for fieldname, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+
+
+class OrderStepForm(forms.ModelForm):
+    class Meta:
+        model = OrderStep
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super(OrderStepForm, self).__init__(*args, **kwargs)
+        for fieldname, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+
+
+class OrderItemForm(forms.ModelForm):
+    class Meta:
+        model = OrderItem
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super(OrderItemForm, self).__init__(*args, **kwargs)
+        for fieldname, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
